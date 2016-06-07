d2506214ba4050a7cdc725fc09b775c9778059ad
aprisun <aprisun@gmail.com>
2016-06-07 23:05:11 +0900

    modified test.bash

--- test.bash
+++ test.bash
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-./test1.bash
-./test2.bash
-./test3.bash
+tmp=/tmp/$$
+
+###### Error Process ######
+ERROR_EXIT () {
+    echo "$1" >&2
+    rm -f $tmp-*
+    exit 1
+}
+
+### test 1 -- normal value## 
+echo 12 24 12 > $tmp-ans
+./gcd.bash 12 24 > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2" 
+
+echo OK
+rm -f $tmp-*
+exit 0
 
 
 
